90ef5358446f1aff1c11a7851f753eb99849e1c0
brutaliccus <jeb32411@gmail.com>
2025-10-16 14:39:42 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,24 @@
-self.addEventListener('install', e => {
-e.waitUntil(caches.open('freilifts-cache').then(cache => cache.addAll(['.', 'index.html', 'manifest.json'])));
+const cacheName = 'workout-log-cache-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
 });
-self.addEventListener('fetch', e => {
-e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
