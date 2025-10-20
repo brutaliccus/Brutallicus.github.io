e9b36e058ee4407058c452de2fe857933b270d12
brutaliccus <jeb32411@gmail.com>
2025-10-20 19:47:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyDSb6HjEPLfkcgUA-MKJxyWwkjdCjZHj2k",
+      authDomain: "freilifts-app.firebaseapp.com",
+      databaseURL: "https://freilifts-app-default-rtdb.firebaseio.com",
+      projectId: "freilifts-app",
+      storageBucket: "freilifts-app.firebasestorage.app",
+      messagingSenderId: "631799148653",
+      appId: "1:631799148653:web:bbf030eba362eb7312cf64"
+    };
+    const USDA_API_KEY = 'aemBTeknGhNmAlKKGpJUiewRCOMdaAVYlAtK91an';
+
 const app = firebase.initializeApp(firebaseConfig);
     const db = firebase.database();
 
@@ -562,3 +575,4 @@ const app = firebase.initializeApp(firebaseConfig);
 
     initializeApp();
 });
+
