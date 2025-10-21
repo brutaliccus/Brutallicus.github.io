4e2b32e6b0d974be5867d09d15266b9b0bc5aa15
brutaliccus <jeb32411@gmail.com>
2025-10-20 21:27:46 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+
+const firebaseConfig = {
+   apiKey: "AIzaSyDSb6HjEPLfkcgUA-MKJxyWwkjdCjZHj2k",
+   authDomain: "freilifts-app.firebaseapp.com",
+   databaseURL: "https://freilifts-app-default-rtdb.firebaseio.com",
+   projectId: "freilifts-app",
+   storageBucket: "freilifts-app.firebasestorage.app",
+   messagingSenderId: "631799148653",
+   appId: "1:631799148653:web:bbf030eba362eb7312cf64"
+};
+
+const USDA_API_KEY = 'aemBTeknGhNmAlKKGpJUiewRCOMdaAVYlAtK91an';
