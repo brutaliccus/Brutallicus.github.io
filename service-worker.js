b8d16c91557c5a4c5c5a23d4be28112aa56bde68
brutaliccus <jeb32411@gmail.com>
2025-10-22 19:39:15 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,43 +4,45 @@ const URLS_TO_CACHE = [
     'index.html',
     'style.css',
     'predefined-exercises.js',
-    'js/app.js',
-    'js/modules/workout.js',
-    'js/modules/exercises.js',
-    'js/modules/food.js',
-    'js/modules/profile.js',
-    'js/modules/progress.js',
-    'js/modules/userAdmin.js'
+    'app.js',
+    'workout.js',
+    'exercises.js',
+    'food.js',
+    'profile.js',
+    'progress.js',
+    'userAdmin.js',
+    'images/icon-192x192.png',
+    'images/icon-512x512.png'
 ];
 
-// Install the service worker and cache all the app's assets
+// --- INSTALL: Cache the application shell ---
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
-                console.log('Opened cache');
+                console.log('Opened cache and caching app shell');
+                // Note: We are using the flattened structure now
                 return cache.addAll(URLS_TO_CACHE);
             })
     );
 });
 
-// Intercept fetch requests and serve from cache first
+// --- FETCH: Serve from cache first (Cache-First Strategy) ---
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                // Cache hit - return response
+                // If the request is in the cache, return it
                 if (response) {
                     return response;
                 }
-                // Not in cache - fetch from network
+                // Otherwise, fetch it from the network
                 return fetch(event.request);
-            }
-        )
+            })
     );
 });
 
-// Clean up old caches when a new service worker activates
+// --- ACTIVATE: Clean up old caches ---
 self.addEventListener('activate', event => {
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
@@ -48,6 +50,7 @@ self.addEventListener('activate', event => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        console.log('Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
                     }
                 })
